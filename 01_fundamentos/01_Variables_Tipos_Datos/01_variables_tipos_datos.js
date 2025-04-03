74231ce21e58b31a2a61f3b3e9946ed2c278cc0c
wbeimar <wbeimarmonsalve33@gmail.com>
2025-04-03 16:29:17 -0500

    Tema 2: Variables y Tipos de Datos en JavaScript

---
+++ 01_fundamentos/01_Variables_Tipos_Datos/01_variables_tipos_datos.js
@@ -0,0 +1,21 @@
+// 📌 Declaración de variables con let y const
+let nombre = "Wbeimar";
+const edad = 25;
+
+// 📌 Tipos de datos primitivos
+let esEstudiante = true;
+let sinValor;
+let datoNulo = null;
+
+// 📌 Objetos y Arrays
+let persona = { nombre: "Ana", edad: 30 };
+let colores = ["rojo", "verde", "azul"];
+
+// 📌 Mostramos en consola
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Es estudiante:", esEstudiante);
+console.log("Sin valor:", sinValor);
+console.log("Dato nulo:", datoNulo);
+console.log("Persona:", persona);
+console.log("Colores:", colores);
---
+++ 01_fundamentos/01_Variables_Tipos_Datos/practica_01.js
@@ -0,0 +1,18 @@
+// 1️⃣ Declarar una variable con tu edad y mostrarla en consola.
+// 2️⃣ Crear un objeto coche con propiedades marca, modelo y año.
+// 3️⃣ Crear un array frutas con 3 nombres de frutas y mostrarlo en consola.
+
+
+let edad = 41;
+console.log("Mi edad es:", edad);
+
+let coche = {
+    marca: "toyota",
+    modelo: "corolla",
+    año: 2025
+};
+console.log(coche);
+
+
+let frutas = ["Banana", "Mango", "Manzana"];
+console.log(frutas);
