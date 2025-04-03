8c7761564997a748f751c22b09da5dc4b3cbe92c
wbeimar <wbeimarmonsalve33@gmail.com>
2025-04-03 16:13:14 -0500

    Ejercicios básicos de JavaScript completados

---
+++ 01_fundamentos/00_Que_es_JavaScript/00_que_es_javascript.js
@@ -0,0 +1,17 @@
+/*
+    📌 ¿Qué es JavaScript?
+    JavaScript es un lenguaje de programación que permite agregar interactividad a los sitios web.
+
+    🔹 Características:
+    - Se ejecuta en el navegador.
+    - No necesita compilación.
+    - Puede manipular el HTML y CSS.
+    - También se usa en servidores con Node.js.
+*/
+
+// Mostrar un mensaje en la consola
+console.log("¡Hola, JavaScript!");
+
+// Declarar una variable y mostrarla en la consola
+let mensaje = "Bienvenido al mundo de JavaScript";
+console.log(mensaje);
---
+++ 01_fundamentos/00_Que_es_JavaScript/practica_00.js
@@ -0,0 +1,10 @@
+//1️⃣ Mostrar un mensaje en consola:
+//Escribe un programa que muestre en la consola el mensaje "¡Aprendiendo JavaScript desde cero!".
+
+console.log("¡Aprendiendo JavaScript desde cero!");
+
+//2️⃣ Crear una variable y mostrar su contenido:
+//Declara una variable llamada miNombre y asígnale tu nombre.Luego, muestra su valor en la consola.
+
+let miNombre = "wbeimar";
+console.log(miNombre);
