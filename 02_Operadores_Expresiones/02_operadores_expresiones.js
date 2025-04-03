da1fbc1ab8bcde57798e3d3366ce21c9dcd92fd2
wbeimar <wbeimarmonsalve33@gmail.com>
2025-04-03 18:08:59 -0500

    Tema 3: Operadores y Expresiones en JavaScript

---
+++ 02_Operadores_Expresiones/02_operadores_expresiones.js
@@ -0,0 +1,25 @@
+// 📌 Operadores aritméticos
+let suma = 10 + 5;
+let resta = 20 - 8;
+let multiplicacion = 4 * 3;
+let division = 16 / 4;
+let modulo = 10 % 3;
+let potencia = 2 ** 3;
+
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicación:", multiplicacion);
+console.log("División:", division);
+console.log("Módulo:", modulo);
+console.log("Potencia:", potencia);
+
+// 📌 Operadores de comparación
+console.log("5 == '5':", 5 == "5"); // true (compara solo el valor)
+console.log("5 === '5':", 5 === "5"); // false (compara valor y tipo)
+
+// 📌 Operadores lógicos
+let a = true,
+    b = false;
+console.log("AND:", a && b);
+console.log("OR:", a || b);
+console.log("NOT:", !a);
---
+++ 02_Operadores_Expresiones/practica_02.js
@@ -0,0 +1,34 @@
+// 📌
+// Ejercicios:
+//  1️⃣ Realizar operaciones aritméticas y mostrar los resultados en consola.
+
+// Crea dos variables num1 y num2 y realiza suma, resta, multiplicación y división con ellas.
+
+// 2️⃣ Usar operadores de comparación para evaluar condiciones.
+
+// Declara una variable edad y usa >= para verificar si una persona es mayor de edad.
+
+// 3️⃣ Usar operadores lógicos para evaluar expresiones.
+
+// Declara dos variables booleanas y usa && , || y!para ver sus resultados en consola.
+
+let num1 = 10;
+let num2 = 5;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+
+console.log("suma:", suma);
+console.log("resta:", resta);
+console.log("multiplicacion:", multiplicacion);
+console.log("division:", division);
+
+console.log("5 == '5':", 5 == "5");
+console.log("5 === '5':", 5 === "5");
+
+let a = true,
+    b = false;
+console.log("AND:", a && b);
+console.log("OR:", a || b);
